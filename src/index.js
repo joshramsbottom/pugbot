19cde9195822f21c62eb0e53168ca6a9d2e4b34a
Joshua Ramsbottom <joshuaramsbottom@electrum.co.za>
2018-01-07 19:20:38 +0200

    Heroku things, add express and keep dyno alive

--- src/index.js
+++ src/index.js
@@ -8,3 +8,27 @@ pugBot.loadCommands(commands);
 pugBot.loadServices();
 pugBot.applyPugsMiddleware();
 pugBot.start();
+
+import http from 'http';
+import express from 'express';
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (request, response) => {
+  // ejs render automatically looks in the views folder
+  response.render('index');
+});
+
+app.listen(port, () => {
+  console.log('App is running on http://localhost:' + port);
+});
+
+// prevent dyno from sleeping
+setInterval(() => {
+  http.get('http://pugbot-ow.herokuapp.com');
+}, 900000);
